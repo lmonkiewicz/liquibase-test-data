8b7e525f72fae7192e3e8a761b53eac7e5dcf2db
Łukasz Monkiewicz <lmonkiewicz@bluesoft.com>
2022-06-10 16:16:49 +0200

    Initial commit

---
+++ src/test/java/pl/effectivedev/example/liquibase/testdata/LiquibaseTestDataApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.effectivedev.example.liquibase.testdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseTestDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
